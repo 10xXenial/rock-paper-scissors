df0964b0378ad6c6e0766bab5865758048be1ee2
EKBaig69 <ebrahim.khurram@gmail.com>
2022-07-26 18:58:54 +0500

    Errors!

--- app.js
+++ app.js
@@ -96,33 +96,63 @@ function playRound(playerSelection, computerSelection) {
 
 //the hardest of them all! the game() function which I just can't figure out!
 
-function game() {
-    let playerScore = 0
-    let compScore = 0
-    for (let round = 0; round < 5; round++) {
-        const pchoice = prompt("What shall you choose?").toLowerCase()
-        const cchoice = getComputerChoice()
-        roundResult = getWinner(pchoice, cchoice)
-        if (roundResult == "player") {
-            playerScore += 1
-        }
-        else if (roundResult == "comp") {
-            compScore += 1
-        }
-        winStatement = playRound(pchoice, cchoice)
-        console.log(winStatement)
-    }
-    if (playerScore > compScore) {
-        console.log(`You Won! You beat the computer ${playerScore} to ${compScore}`)
-    }
-    else if (playerScore < compScore) {
-        console.log(`You Lost! The computer beat you ${compScore} to ${playerScore}`)
-    }
-    else {
-        console.log(`It's a tie! You both finished with a score of ${playerScore}`)
-    }
+// function game() {
+    // let playerScore = 0
+    // let compScore = 0
+    // for (let round = 0; round < 5; round++) {
+    //     const pchoice = prompt("What shall you choose?").toLowerCase()
+    //     const cchoice = getComputerChoice()
+    //     roundResult = getWinner(pchoice, cchoice)
+    //     if (roundResult == "player") {
+    //         playerScore += 1
+    //     }
+    //     else if (roundResult == "comp") {
+    //         compScore += 1
+    //     }
+    //     winStatement = playRound(pchoice, cchoice)
+    //     console.log(winStatement)
+    
+    // if (playerScore > compScore) {
+    //     console.log(`You Won! You beat the computer ${playerScore} to ${compScore}`)
+    // }
+    // else if (playerScore < compScore) {
+    //     console.log(`You Lost! The computer beat you ${compScore} to ${playerScore}`)
+    // }
+    // else {
+    //     console.log(`It's a tie! You both finished with a score of ${playerScore}`)
+    // }
+
+const btnRock = document.querySelector('#rock')
+const btnPaper = document.querySelector('#paper')
+const btnScissors = document.querySelector('#scissors')
+const winAnnouncer = document.querySelector('div')
+
+function click (btnN) {
+    const pchoice = btnN
+    const cchoice = getComputerChoice()
+    const winstate = playRound(pchoice, cchoice)
+    const winner = getWinner()
+    winAnnouncer.textContent = winstate
+}
+let timesPlayed = 0;
+while (timesPlayed <= 5) {
+    btnRock.addEventListener('click', () => {
+        let buttonChosen = 'rock'
+        click(buttonChosen)
+        timesPlayed++
+    } )
+    btnPaper.addEventListener('click', () => {
+        let buttonChosen = 'paper'
+        click(buttonChosen)
+        timesPlayed++
+    } )
+    btnScissors.addEventListener('click', () => {
+        let buttonChosen = 'scissor'
+        click(buttonChosen)
+        timesPlayed++
+    } )
 }
 
-game();
 
 //I am freaking so happy! I finally got this code right! that's the beauty of coding!
+//FUTURE ME: DAMN! THIS JS IS REALLY FRICKIN' GETTTING TO ME!
